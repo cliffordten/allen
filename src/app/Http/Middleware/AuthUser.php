7d281bf019e569f5d514e2032e5ce8bbb7506a26
cliffordten <cliffordteng5@gmail.com>
2022-06-05 02:06:34 +0100

    fixed issue when user is logged out from dashboard and clicks on browser back button to go back

--- src/app/Http/Middleware/AuthUser.php
+++ src/app/Http/Middleware/AuthUser.php
@@ -38,6 +38,9 @@ class AuthUser
             
         }
 
-        return $next($request);
+        return $next($request)->header('Cache-Control', 'no-cache, no-store, max-age=0, must-revalidate')
+                              ->header('Pragma', 'no-cache')
+                              ->header('Expires', 'Sat 01 Jan 1990 00:00:00 GMT');
+        
     }
 }
